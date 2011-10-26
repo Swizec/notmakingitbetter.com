47097cb512641519d221dcf334d8629ebf8c9f0c
Swizec Teller <swizec@swizec.com>
2011-10-27 00:45:54 +0200

    working notify script

--- notify_shipped.js
+++ notify_shipped.js
@@ -1,17 +1,22 @@
 
 var _ = require('underscore');
 var mongodb = require('mongodb');
-var settings = require('../settings');
+var settings = require('./settings');
 var mongo = new mongodb.Db((settings.dev) ? 'dev-postcards' : 'postcards',
                            new mongodb.Server('127.0.0.1', 27017, {}));
+
+var notifications = require('./lib/notifications');
 
 if (!module.parent) {
     mongo.open(function (err) {
         var transactions = new mongodb.Collection(mongo, 'transactions');
 
         transactions.find({shipped: true}).toArray(function (err, data) {
-
+            data.map(function (payment) {
+               notifications.email('shipped',
+                                    payment);
+            });
+            mongo.close();
         });
-
     });
 }
